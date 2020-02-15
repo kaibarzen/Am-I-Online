2b0038e4ce7a1ff8e8e544ca120d1dcb6e95d9f5
Kai Barzen <52884225+kaibarzen@users.noreply.github.com>
2020-02-15 19:33:08 +0100

    serverless cors

--- serverless/src/online.js
+++ serverless/src/online.js
@@ -10,6 +10,10 @@ const online = async (event, context) =>
 {
 	return {
 		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': true,
+		},
 		body: JSON.stringify(
 			{
 				online: true,
