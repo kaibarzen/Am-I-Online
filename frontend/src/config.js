e80bf0580923154d5b54dfa60e8790778744debd
Kai Barzen <52884225+kaibarzen@users.noreply.github.com>
2020-02-15 21:15:03 +0100

    Fixed Manifest; Fixed aws region

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 export default {
-	endpoint: 'https://g6op1mvayh.execute-api.eu-central-1.amazonaws.com/production/online', // We Could also use an network change event instead but then our api would be totally useless! :(
+	endpoint: 'https://h2o22yvohk.execute-api.eu-west-1.amazonaws.com/prod/online', // We Could also use an network change event instead but then our api would be totally useless! :(
 	timeout: 1000 * 2, // Timeout for http request in ms
 	interval: 1000 * 10, // Check every ms for an connection
 };
